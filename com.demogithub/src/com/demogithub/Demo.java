6f40a65110aa88733a3d6c078f682c2fc0c0d35a
Mahavir Maraje <bipin.mahavir-maraje@capgemini.com>
2023-05-22 12:30:43 +0530

    Added

--- com.demogithub/src/com/demogithub/Demo.java
+++ com.demogithub/src/com/demogithub/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Hi");
+		System.out.println("Hello");
 	}
 
 }
